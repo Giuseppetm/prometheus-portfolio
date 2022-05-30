eb583f7403a358e83accbc937ea8f33de0197401
Giuseppetm <giuseppe.delcampo@outlook.com>
2022-05-30 21:24:14 +0200

    Project setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import MouseFollower from "mouse-follower";
+import gsap from "gsap";
+import './index.scss';
+
+MouseFollower.registerGSAP(gsap);
+
+// eslint-disable-next-line no-unused-vars
+const cursor = new MouseFollower();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
